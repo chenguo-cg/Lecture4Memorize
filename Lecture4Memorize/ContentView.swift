c84fb0afbfd9dc176cfca860449998279bc398b6
guochen <chenguo0313@gmail.com>
2022-10-14 00:25:49 -0400

    Lecture 4 Code

--- Lecture4Memorize/ContentView.swift
+++ Lecture4Memorize/ContentView.swift
@@ -8,14 +8,55 @@
 import SwiftUI
 
 struct ContentView: View {
+    // @ObservedObject: when viewModel says sth changed, plz rebuild my entire body
+    @ObservedObject var viewModel: EmojiMemoryGame
+    
+    //shows what's in the model
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ScrollView {
+            LazyVGrid(columns: [GridItem(.adaptive(minimum: 65))]) {
+                ForEach(viewModel.cards) { card in // bag of lego View!
+                    CardView(card: card)
+                        .aspectRatio(2/3, contentMode: .fit)
+                        .onTapGesture {
+                            // ask viewModel to express User intent
+                            viewModel.choose(card)
+                        }
+                }
+            }
+        }
+        .foregroundColor(.red)
+        .padding(.horizontal)
     }
 }
 
+// View that shows what the card looks like
+struct CardView: View {
+    let card: MemoryGame<String>.Card
+    
+    var body: some View {
+        ZStack {
+            let shape = RoundedRectangle(cornerRadius: 20)
+            if card.isFaceUp {
+                shape.fill().foregroundColor(.white)
+                shape.strokeBorder(lineWidth: 3)
+                Text(card.content).font(.largeTitle)
+            } else if card.isMatched {
+                shape.opacity(0)
+            } else {
+                shape.fill()
+            }
+        }
+    }
+}
+
+
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        let game = EmojiMemoryGame()
+        ContentView(viewModel: game)
+            .preferredColorScheme(.dark)
+        ContentView(viewModel: game)
+            .preferredColorScheme(.light)
     }
 }
