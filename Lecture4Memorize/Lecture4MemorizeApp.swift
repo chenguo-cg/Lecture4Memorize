144b582949f05f57b2bb144e36db480146910746
guochen <chenguo0313@gmail.com>
2022-10-14 00:20:36 -0400

    Initial Commit

---
+++ Lecture4Memorize/Lecture4MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lecture4MemorizeApp.swift
+//  Lecture4Memorize
+//
+//  Created by Guo Chen on 10/14/22.
+//
+
+import SwiftUI
+
+@main
+struct Lecture4MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
